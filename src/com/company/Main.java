8d3cd68c7012364a9f8448f0ebe602ce82973065
meu? <dvd.filmas@gmail.com>
2019-09-15 20:07:55 +0300

    Feature allowing to choose range of symbols for patterns

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,17 @@
 package com.company;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
         int[] newData = {
-                1, 2, 3, 2, 3, 17, 4, 1, 17, 2, 3, 1, 2, 3};//
-              //0, 1, 2, 3, 4, 5,  6, 7,  8, 9, 10,11,12,13, 14, 15
-        Boolean doWeLookForCertainSymbols = true;
+                1, 2, 3, 17, 1, 2, 3, 4, 20, 2, 3, 4};//
+        //0, 1, 2, 3, 4, 5,  6, 7,  8, 9, 10,11,12,13, 14, 15
+        boolean doWeLookForCertainSymbols = true;
         int[] symbols = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         List<String> db = new ArrayList<>();
         HashMap<String, HashSet<Integer>> patterns = new HashMap<>();
@@ -28,16 +31,18 @@ public class Main {
      */
     public void incommingDataFlow(int[] newData, List<String> db, int[] symbols, HashMap<String, HashSet<Integer>> patterns, boolean doWeLookForCertainSymbols) {
 
-        //  putAllowedSymbolsToDB(symbols, patterns);
         for (int i = 0; i < newData.length; i++) {
             String currentSymbolString = "" + newData[i];
-            System.out.println("gavau nauja skaiciu " + currentSymbolString + ". tai yra " + (i + 1) + "/" + (newData.length + 1) + " simbolis");
-             boolean validSymbol = true ;
-            if(doWeLookForCertainSymbols)isCompatableWithAllowedSymbols(symbols, currentSymbolString);
+            System.out.println("gavau nauja skaiciu " + currentSymbolString + ". tai yra " + (i + 1) + "/" + (newData.length) + " simbolis");
+            boolean validSymbol = true;
+            if (doWeLookForCertainSymbols) validSymbol = isCompatableWithAllowedSymbols(symbols, currentSymbolString);
             addNewDataToDB(db, currentSymbolString);
-            if (!validSymbol) {System.out.println("blogas simbolis"); continue;}
-            addPatternAndCount(patterns, db, currentSymbolString);
-            soutKnownPatterns(patterns);
+            if (!validSymbol) {
+                System.out.println("blogas simbolis");
+                continue;
+            }
+            addPatternAndCount(patterns, db, currentSymbolString, symbols);
+            printAllKnownPatterns(patterns);
             try {
                 Thread.sleep(700);
             } catch (InterruptedException e) {
@@ -46,7 +51,7 @@ public class Main {
         }
     }
 
-    public void soutKnownPatterns(HashMap<String, HashSet<Integer>> patterns) {
+    public void printAllKnownPatterns(HashMap<String, HashSet<Integer>> patterns) {
         for (String pattern : patterns.keySet()) {
             if (patterns.get(pattern).size() > 1) {//lyg
                 System.out.println("patternas " + pattern + " pasikartojo pozicijose: " + patterns.get(pattern));
@@ -55,47 +60,25 @@ public class Main {
     }
 
     public boolean isCompatableWithAllowedSymbols(int[] symbols, String currentSymbol) {
-        Boolean bl = false;
+        boolean bl = false;
         for (int symbol = 0; symbol < symbols.length; symbol++) {
-           if (currentSymbol.equals(symbols[symbol])){
-               bl=true;
-               break;
+            if (currentSymbol.equals("" + symbols[symbol])) {
+                bl = true;
+                return bl;
             }
         }
-//        while (!bl) {
-//            for (int symbol = 0; symbol < symbols.length; symbol++) {
-//                if (isSymbolValid(symbol, symbols, currentSymbol)) {
-//                    bl = true;
-//                }
-//            }
-//        }
         return bl;
-    }
-
-    public boolean isSymbolValid(int symbol, int[] symbols, String currentSymbol) {
-        if (currentSymbol.equals("" + symbols[symbol])) {
-            return true;
-        }
-        return false;
     }
 
     public void addNewDataToDB(List<String> db, String currentSymbol) {
         db.add(currentSymbol);
     }
 
-    public void putAllowedSymbolsToDB(int[] symbols, HashMap<String, HashSet<Integer>> patterns) {
-        for (int a = 0; a < symbols.length; a++) {
-            patterns.put("" + symbols[a], new HashSet<>());
-        }
-    }
-
-    public void addPatternAndCount(HashMap<String, HashSet<Integer>> patterns, List<String> db, String currentSymbolString) {
+    public void addPatternAndCount(HashMap<String, HashSet<Integer>> patterns, List<String> db, String currentSymbolString, int[] symbols) {
         System.out.println("pasileido addPatternAndCount");
         String pattern = "";
         for (int h = db.size(); h-- > 0; ) {
-            // System.out.println("db.size="+db.size()+",h="+h);
-            //if (h == 0) break;
-
+            if (!isCompatableWithAllowedSymbols(symbols, db.get(h))) break;
             pattern = db.get(h) + "," + pattern;
             if (h == db.size() - 1) {
                 pattern = db.get(h);
